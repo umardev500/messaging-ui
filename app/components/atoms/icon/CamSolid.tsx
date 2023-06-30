166a79f6281ba675622dda308ec483e945067527
umar <umar.dev500@gmail.com>
2023-06-30 10:24:42 +0700

    add icon for tab

---
+++ app/components/atoms/icon/CamSolid.tsx
@@ -0,0 +1,17 @@
+import { colors } from '@app/constants';
+import * as React from 'react';
+import { View } from 'react-native';
+import { Path, Svg } from 'react-native-svg';
+
+interface Props {
+  color?: string;
+  size?: number;
+}
+
+export const CamSolid: React.FC<Props> = ({ color = colors.gray[600], size = 24 }) => (
+  <View>
+    <Svg width={size} height={size} fill={color} stroke={color} strokeWidth={1.5} viewBox="0 0 24 24">
+      <Path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5zm15.44 14.25-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06z" />
+    </Svg>
+  </View>
+);
--- app/components/organisms/navigation/MainTabs.tsx
+++ app/components/organisms/navigation/MainTabs.tsx
@@ -1,18 +1,68 @@
+import { CamSolid, ChatSolid, PeopleSolid } from '@app/components/atoms';
 import { HomePage } from '@app/components/pages';
+import { colors } from '@app/constants';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 const Tab = createBottomTabNavigator();
 
 export const MainTabs: React.FC = () => {
+  const renderChatMenu = useCallback((props: { focused: boolean; color: string; size: number }) => {
+    return <ChatSolid size={20} color={props.color} />;
+  }, []);
+
+  const renderCamMenu = useCallback((props: { focused: boolean; color: string; size: number }) => {
+    return <CamSolid size={20} color={props.color} />;
+  }, []);
+
+  const renderPeopleMenu = useCallback((props: { focused: boolean; color: string; size: number }) => {
+    return <PeopleSolid size={20} color={props.color} />;
+  }, []);
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarInactiveTintColor: colors.gray[400],
+        tabBarActiveTintColor: colors.red[600],
+        tabBarLabelStyle: {
+          fontSize: 14,
+        },
+        tabBarStyle: {
+          paddingTop: 10,
+          height: 65,
+          paddingBottom: 8,
+        },
+        tabBarIconStyle: {},
+        tabBarItemStyle: {
+          marginRight: 10,
+          gap: 0,
+        },
+      }}>
       <Tab.Screen
         options={{
+          tabBarIcon: renderChatMenu,
           headerShown: false,
-          tabBarShowLabel: false,
+          title: 'Chats',
         }}
         name="HomeTab"
+        component={HomePage}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: renderCamMenu,
+          headerShown: false,
+          title: 'Calls',
+        }}
+        name="CallTab"
+        component={HomePage}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: renderPeopleMenu,
+          headerShown: false,
+          title: 'People',
+        }}
+        name="PeopleTab"
         component={HomePage}
       />
     </Tab.Navigator>
